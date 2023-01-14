e2c8af7669a7eacde0f7a32667782cf48cd97d8d
thiagofportella <thiagofportella@gmail.com>
2023-01-14 20:16:19 -0300

    feat: nft creation

---
+++ lib/wallet_manager/operations/nft_creator.rb
@@ -0,0 +1,52 @@
+module WalletManager
+  module Operations
+    class NftCreator < Base
+      def initialize(network_url:, credentials:, data:, sandbox: !Rails.env.production?)
+        super(network_url:, sandbox:)
+        @credentials = credentials
+        @data = data
+      end
+
+      def create
+        {
+          asset_id: response_body['asset_id'],
+          create_transaction_id: response_body['create_transaction_id'],
+          create_transaction_outputs: response_body['create_transaction_outputs']
+        }
+      end
+
+      private
+
+      attr_reader :credentials, :data
+
+      def response_body
+        @response_body ||= response.body
+      end
+
+      def response
+        post('/asset', header_params:, params: body_params)
+      end
+
+      def body_params
+        {
+          amount: 1,
+          nft: nft_data
+        }
+      end
+
+      def nft_data
+        nft_data = {}
+        nft_data['data'] = { asset: data }
+
+        nft_data
+      end
+
+      def header_params
+        {
+          'Public-Key' => credentials.public_key,
+          'Private-Key' => credentials.private_key
+        }
+      end
+    end
+  end
+end
---
+++ spec/wallet_manager/operations/nft_creator_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe WalletManager::Operations::NftCreator do
+  describe '#create', vcr: { cassette_name: 'wallet_manager/operations/nft_creator' } do
+    it 'returns recently created asset id' do
+      network_url = Rails.application.credentials.default_bigchain_network_url
+      credentials = instance_double(Credential,
+                                    public_key: 'AuVTSSLZpi4pbQmUZN1Zx41EuRNLiQ3UWgeiHxY2CfPv',
+                                    private_key: '2N3adpwMwWLss3i4xfVKK4Jq1EDPXXNTSMjBNnkH7Pqy')
+
+      data = { nft1_code: 'foobar' }
+
+      response = described_class.new(network_url:, credentials:, data:).create
+      expect(response[:asset_id]).to be_an_instance_of(String)
+    end
+  end
+end
