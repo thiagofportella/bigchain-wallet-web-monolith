dbef6f7227965c35634ab119337a984bf138983c
thiagofportella <thiagofportella@gmail.com>
2023-01-14 18:36:32 -0300

    chore: lib path to initialization

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,14 @@
-require_relative "boot"
+require_relative 'boot'
+require 'rails/all'
 
-require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module BigchainWalletWebMonolith
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Brasilia'
+    config.autoload_paths   << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
