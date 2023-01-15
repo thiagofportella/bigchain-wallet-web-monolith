61d12625fd43e6bfd1b740d392b429499c42397a
thiagofportella <thiagofportella@gmail.com>
2023-01-15 01:43:54 -0300

    refactor: current user for credentials controller

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -2,8 +2,7 @@ class CredentialsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @user = User.find(params[:user_id])
-    @credential = @user.generate_credentials
+    @credential = current_user.generate_credentials
 
     flash[:notice] = 'Credenciais geradas com sucesso!'
     redirect_to root_path
